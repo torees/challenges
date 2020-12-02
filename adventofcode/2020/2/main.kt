7c7145af3ccf1cb50d2fea5a4d8817703fc29b63
Tore Sæterdal <tore.1992@gmail.com>
2020-12-02 19:08:40 +0100

    added solution for day 2

--- adventofcode/2020/2/main.kt
+++ adventofcode/2020/2/main.kt
@@ -1,6 +1,51 @@
 
+import java.io.File
+import java.io.BufferedReader
 
+//val inputpasswords = listOf("1-3 a: abcde","1-13 b: cdefg","2-9 c: ccccccccc")
 
 fun main(){
-    print("Hello\n")
+    val inputfile = "input.txt"
+    //Create mutable list object
+    val inputpasswords = mutableListOf<String>()
+    //Create BufferedReader
+    val bufferedReader = File(inputfile).bufferedReader()
+    //Read each line into list object, with int casting
+    bufferedReader.useLines { lines -> lines.forEach { inputpasswords.add(it) } }
+    println("Valid passwords: "+ part1(inputpasswords))
+    println("Really Valid passwords: "+ part2(inputpasswords))
+}
+
+fun part1(inputpasswords: MutableList<String>): Int{
+    var totalValid = 0;
+
+    for ( line in 0 until inputpasswords.size){
+        val str = inputpasswords[line]
+        var key = str[str.indexOf(":") - 1]
+        var lowerlim = str.substringBefore(delimiter = "-").toInt()
+        var upperlim = str.substringBefore(delimiter = " ").substringAfter(delimiter = "-").toInt()
+        val password  = str.substringAfter(delimiter = ": ")
+        val count = password.count { it == key.toChar() }
+        if(count in lowerlim..upperlim)totalValid=totalValid+1;
+    }
+    return totalValid
+}
+fun part2(inputpasswords: MutableList<String>): Int{
+    var totalValid = 0;
+    for ( line in 0 until inputpasswords.size){
+        val str = inputpasswords[line]
+        var key = str[str.indexOf(":") - 1]
+        var lowerlim = str.substringBefore(delimiter = "-").toInt()
+        var upperlim = str.substringBefore(delimiter = " ").substringAfter(delimiter = "-").toInt()
+        val password  = str.substringAfter(delimiter = ": ")
+        if ( password[lowerlim-1 ] == key || password[upperlim-1]== key) {
+            if (password[upperlim-1] != password[lowerlim-1]){
+                totalValid=totalValid+1;
+            }
+        }
+        
+        
+        
+    }
+    return totalValid
 }
